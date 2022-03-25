8808ed7cb8a036b095137ceda828e1321d05ab01
Umer Kang <umerkang66@gmail.com>
2022-03-25 20:46:01 +0500

    again convert the project to version 1

--- react-and-typescript-with-a-complex-app-stephen-grider/jbook/packages/cli/src/commands/serve.ts
+++ react-and-typescript-with-a-complex-app-stephen-grider/jbook/packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from '@js-note-umer/local-api';
+import { serve } from '@jbook-umer/local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
