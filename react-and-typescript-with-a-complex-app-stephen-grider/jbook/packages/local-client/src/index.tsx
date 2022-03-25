f25c077f37683d769da5feb8d793f07f939437d0
Umer Kang <umerkang66@gmail.com>
2022-03-25 20:15:59 +0500

    add two commands for lerna

--- react-and-typescript-with-a-complex-app-stephen-grider/jbook/packages/local-client/src/index.tsx
+++ react-and-typescript-with-a-complex-app-stephen-grider/jbook/packages/local-client/src/index.tsx
@@ -29,3 +29,5 @@ ReactDOM.render(
 // lerna add local-api --scope=cli
 // Execute the start scripts in each different packages
 // lerna run start  --parallel
+// lerna bootstrap
+// lerna publish --no-push
