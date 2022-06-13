5426e872239ea6741282d87560e32bae49ab72f7
umerkang66 <umerkang66@gmail.com>
2022-06-13 16:33:09 +0500

    refactored project for supertest jest testing environment

--- microservices-stephen-grider/ticketing/auth/src/index.ts
+++ microservices-stephen-grider/ticketing/auth/src/index.ts
@@ -1,45 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-// Routes
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-// Errors
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// traffic is being proxy to our server through ingress nginx, and trust the proxies
-app.set('trust proxy', true);
-app.use(express.json());
-
-app.use(
-  cookieSession({
-    // don't encrypt this
-    signed: false,
-    // only set cookies on https
-    secure: true,
-  })
-);
-
-// Routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', (req, res) => {
-  const errMsg = `'${req.method}: ${req.originalUrl}' does not found on this server`;
-  throw new NotFoundError(errMsg);
-});
-
-// Error Handler middleware
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
