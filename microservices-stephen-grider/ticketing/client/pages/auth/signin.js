9bee1168b9c7369e445406f97312f331dfbb03a4
umerkang66 <umerkang66@gmail.com>
2022-06-14 09:33:10 +0500

    added client for auth

--- microservices-stephen-grider/ticketing/client/pages/auth/signin.js
+++ microservices-stephen-grider/ticketing/client/pages/auth/signin.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const SignUp = () => {
+const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const { doRequest, errors } = useRequest({
-    url: '/api/users/signup',
+    url: '/api/users/signin',
     method: 'post',
     body: { email, password },
     onSuccess: () => Router.push('/'),
@@ -20,7 +20,7 @@ const SignUp = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
+      <h1>Sign in</h1>
       <div className="form-group">
         <label htmlFor="email">Email address</label>
         <input
@@ -46,10 +46,10 @@ const SignUp = () => {
       {errors}
 
       <button type="submit" className="btn btn-primary">
-        Sign up
+        Sign in
       </button>
     </form>
   );
 };
 
-export default SignUp;
+export default SignIn;
