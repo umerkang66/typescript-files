a2e2695cb66d6561f4fb392909aa1efa9546a142
umerkang66 <umerkang66@gmail.com>
2022-08-07 20:35:07 +0500

    removed ticketing from typescript files

--- microservices-stephen-grider/ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Publisher,
-  Subjects,
-  TicketCreatedEvent,
-} from '@ticketing-umer/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  protected readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
--- microservices-stephen-grider/ticketing/tickets/src/events/publishers/ticket-updated-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Publisher,
-  Subjects,
-  TicketUpdatedEvent,
-} from '@ticketing-umer/common';
-
-export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
-  protected readonly subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-}
